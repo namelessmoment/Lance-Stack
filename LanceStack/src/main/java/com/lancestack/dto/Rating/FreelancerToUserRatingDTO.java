af4f8c184fbfcb7bed630b814151f773088ae963
Mohit <mohitdk2@gmail.com>
2024-08-07 23:23:19 +0530

    Rating Bug Fixed

--- LanceStack/src/main/java/com/lancestack/dto/Rating/FreelancerToUserRatingDTO.java
+++ LanceStack/src/main/java/com/lancestack/dto/Rating/FreelancerToUserRatingDTO.java
@@ -12,6 +12,6 @@ public class FreelancerToUserRatingDTO {
 	private BigDecimal  rating;
 	private String feedback;
     private Long contract;
-    private Long ratee;  //User
-    private Long rater; //Freelancer
+    private Long user;  //User
+    private Long freelancer; //Freelancer
 }
--- LanceStack/src/main/java/com/lancestack/service/RatingServiceImpl.java
+++ LanceStack/src/main/java/com/lancestack/service/RatingServiceImpl.java
@@ -1,8 +1,9 @@
 package com.lancestack.service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
 
-import org.apache.catalina.mapper.Mapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -51,13 +52,13 @@ public class RatingServiceImpl implements RatingService {
 		else {
 			Contract contract =contractRepo.findById(ratingDTO.getContract())
 					.orElseThrow();
-			User user = userRepo.findById(ratingDTO.getRater()).orElseThrow();
-			Freelancer freelancer = freelancerRepo.findById(ratingDTO.getRatee())
+			User user = userRepo.findById(ratingDTO.getUser()).orElseThrow();
+			Freelancer freelancer = freelancerRepo.findById(ratingDTO.getFreelancer())
 					.orElseThrow();
 			Rating rating = modelMapper.map(ratingDTO, Rating.class);
 			rating.setContract(contract);
-			rating.setRater(user);
-			rating.setRatee(freelancer);
+			rating.setUser(user);
+			rating.setFreelancer(freelancer);
 			rating.setRatingType(RatingType.USER_TO_FREELANCER);
 			ratingRepo.save(rating);
 			msg = "Rating To Freelancer is Successful";
@@ -74,13 +75,13 @@ public class RatingServiceImpl implements RatingService {
 		else {
 			Contract contract =contractRepo.findById(ratingDTO.getContract())
 					.orElseThrow();
-			User user = userRepo.findById(ratingDTO.getRatee()).orElseThrow();
-			Freelancer freelancer = freelancerRepo.findById(ratingDTO.getRater())
+			User user = userRepo.findById(ratingDTO.getUser()).orElseThrow();
+			Freelancer freelancer = freelancerRepo.findById(ratingDTO.getFreelancer())
 					.orElseThrow();
 			Rating rating = modelMapper.map(ratingDTO, Rating.class);
 			rating.setContract(contract);
-			rating.setRater(user);
-			rating.setRatee(freelancer);
+			rating.setUser(user);
+			rating.setFreelancer(freelancer);
 			rating.setRatingType(RatingType.FREELANCER_TO_USER);
 			ratingRepo.save(rating);
 			msg = "Rating To User is Successful";
@@ -90,16 +91,40 @@ public class RatingServiceImpl implements RatingService {
 
 	@Override
 	public BigDecimal getAverageRatingOfFreelancer(Long freelancerId) {
-			Freelancer freelancer = freelancerRepo.findById(freelancerId).orElseThrow(()-> new ResourceNotFound("No user found"));
+//			Freelancer freelancer = freelancerRepo.findById(freelancerId).orElseThrow(()-> new ResourceNotFound("No user found"));
 //			modelMapper()
 			
 //		if(userId==null) {
 //			throw new RuntimeException("Invalid UserId");
 //		}
 //		else {
-			return ratingRepo.findAverageRatingByFreelancerId(freelancer);
+//			return ratingRepo.findAverageRatingByFreelancerId(freelancer);
+		Freelancer freelancer = freelancerRepo.findById(freelancerId).orElseThrow();
+		List<Rating> ratings = ratingRepo.findByFreelancer(freelancer);
+	    return calculateAverageRating(ratings);
+
 //		}
 	}
+	
+	@Override
+	public BigDecimal getAverageRatingForUser(Long userId) {
+		User user = userRepo.findById(userId).orElseThrow();
+	    List<Rating> ratings = ratingRepo.findByUser(user);
+	    return calculateAverageRating(ratings);
+	}
+
+	// to calculate rating
+	private BigDecimal calculateAverageRating(List<Rating> ratings) {
+	    if (ratings.isEmpty()) {
+	        return BigDecimal.ZERO;
+	    }
+	    BigDecimal sum = BigDecimal.ZERO;
+	    for (Rating rating : ratings) {
+	        sum = sum.add(rating.getRating());
+	    }
+	    return sum.divide(BigDecimal.valueOf(ratings.size()), 2, RoundingMode.HALF_UP);
+	}
+
 	
 	
 }
