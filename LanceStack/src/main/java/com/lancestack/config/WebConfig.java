b1f03f488d06411d713bbd8f687fbcd9c4d32536
pratik <pratikk2201@gmail.com>
2024-08-12 11:27:46 +0530

    new add

--- LanceStack/src/main/java/com/lancestack/config/WebConfig.java
+++ LanceStack/src/main/java/com/lancestack/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
